9691782a3a6f48cc724de372370fe2c40889cd7a
dewomser <webmaster@untergang.de>
2021-12-01 18:15:13 +0100

    Github Isue eingebaut

---
+++ _layouts/utteranc.js
@@ -0,0 +1,8 @@
+<script src="https://utteranc.es/client.js"
+        repo="Wolust/wolust.github.io"
+        issue-term="pathname"
+        label="Linux"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
