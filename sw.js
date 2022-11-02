f96cab4a1e0f08392914e41391da40a4283acba5
Stefan Hoehn <webmaster@untergang.de>
2022-11-02 19:01:30 +0100

    restlichen googlefonts gelöscht

--- sw.js
+++ sw.js
@@ -35,10 +35,10 @@ addEventListener("fetch", function(e) {
         return response || fetch(e.request).then(function(response) {
         var clonedResponse = response.clone();
         var hosts = [
-          "https://fonts.googleapis.com",
-          "https://fonts.gstatic.com",
-          "https://maxcdn.bootstrapcdn.com",
-          "https://cdnjs.cloudflare.com"
+         // "https://fonts.googleapis.com",
+         // "https://fonts.gstatic.com",
+         // "https://maxcdn.bootstrapcdn.com",
+         // "https://cdnjs.cloudflare.com"
         ];
         hosts.map(function(host) {
           if (e.request.url.indexOf(host) === 0) {
