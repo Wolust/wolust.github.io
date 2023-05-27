699046f0eed7726af13ece44d4987614a501f7c5
Stefan Hoehn <webmaster@untergang.de>
2023-05-27 12:04:50 +0200

    Linux prentationdasy 26.05.2023

--- vendor-alt/bundle/ruby/3.0.0/gems/jekyll-admin-0.11.1/lib/jekyll-admin/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module JekyllAdmin
-  VERSION = "0.11.1"
-end
--- vendor-alt/bundle/ruby/3.0.0/gems/sinatra-contrib-3.0.5/lib/sinatra/contrib/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sinatra
-  module Contrib
-    VERSION = '3.0.4'
-  end
-end
